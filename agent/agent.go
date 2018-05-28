f631df99f51c408aaca723d55865603261c5aecd
Lars Sjöström <lars@radicore.se>
2018-05-28 12:10:35 +0200

    agent: preform login

--- agent/agent.go
+++ agent/agent.go
@@ -1,23 +1,35 @@
 package agent
 
 import (
-	"context"
 	"crypto/tls"
 	"fmt"
-	"log"
 
 	"github.com/dln/luxaforce/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
 )
 
-func newLuxaforceClient(hostAndPort) (api.LuxaforceClient, error) {
-	conn, err := grpc.Dial(hostAndPort, grpc.WithTransportCredentials(
-		credentials.NewTLS(&tls.Config{InsecureSkipVerify: true}),
-	))
+func NewLuxaforceClient(hostAndPort string) (api.LuxaforceClient, error) {
+	login := &LoginAgent{
+		AllowBrowser: true,
+	}
+
+	token, err := login.PerformLogin()
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := grpc.Dial(hostAndPort, []grpc.DialOption{
+		grpc.WithPerRPCCredentials(oauth.NewOauthAccess(token)),
+		grpc.WithTransportCredentials(
+			credentials.NewTLS(&tls.Config{InsecureSkipVerify: true}),
+		),
+	}...)
 	if err != nil {
 		return nil, fmt.Errorf("dail: %v", err)
 	}
+
 	return api.NewLuxaforceClient(conn), nil
 }
 
